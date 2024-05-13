7f0be01af4f5f77a7b90e874175762f97f6694c3
liuyulvv <liuyulvv@outlook.com>
2024-05-13 10:56:22 +0800

    add: impl intersection

--- src/topo/segment_2.rs
+++ src/topo/segment_2.rs
@@ -42,6 +42,25 @@ impl Segment2 {
         let cb = self.end - other.start;
         ab.cross(&ac) * ab.cross(&ad) < eps && cd.cross(&ca) * cd.cross(&cb) < eps
     }
+
+    /// TODO: implement overlap
+    /// overlay is not implemented
+    pub fn intersection(&self, other: &Self, eps: Option<Eps>) -> Option<Point2> {
+        if !self.is_intersect(other, eps) {
+            return None;
+        }
+        let eps = eps.unwrap_or(Eps::default()).value;
+        let ab = self.end - self.start;
+        let cd = other.end - other.start;
+        let ac = other.start - self.start;
+        let cross_ab_cd = ab.cross(&cd);
+        if cross_ab_cd.abs() < eps || cross_ab_cd.is_nan() {
+            return None;
+        }
+        let t = ac.cross(&cd) / ab.cross(&cd);
+        let res = self.start.get_vector() + ab * t;
+        return Some(Point2::from_vector(&res));
+    }
 }
 
 impl IsParallel for Segment2 {
@@ -196,5 +215,57 @@ mod tests {
         let s2 = Segment2::new(p3, p4);
         let result = s1.is_intersect(&s2, None);
         assert_eq!(result, false);
+    }
+
+    #[test]
+    fn intersection() {
+        let p1 = Point2::new(1.0, 1.0);
+        let p2 = Point2::new(1.0, 1.0);
+        let s1 = Segment2::new(p1, p2);
+        let s2 = Segment2::new(p1, p2);
+        let result = s1.intersection(&s2, None);
+        assert_eq!(result, None);
+
+        let p1 = Point2::new(1.0, 1.0);
+        let p2 = Point2::new(1.0, 2.0);
+        let s1 = Segment2::new(p1, p2);
+        let s2 = Segment2::new(p1, p2);
+        let result = s1.intersection(&s2, None);
+        assert_eq!(result, None);
+
+        let p1 = Point2::new(1.0, 1.0);
+        let p2 = Point2::new(1.0, 1.1);
+        let p3 = Point2::new(1.1, 1.2);
+        let s1 = Segment2::new(p1, p2);
+        let s2 = Segment2::new(p2, p3);
+        let result = s1.intersection(&s2, None);
+        assert_eq!(result, Some(Point2::new(1.0, 1.1)));
+
+        let p1 = Point2::new(1.0, 1.0);
+        let p2 = Point2::new(1.0, 1.1);
+        let p3 = Point2::new(1.1, 1.2);
+        let p4 = Point2::new(1.2, 1.3);
+        let s1 = Segment2::new(p1, p2);
+        let s2 = Segment2::new(p3, p4);
+        let result = s1.intersection(&s2, None);
+        assert_eq!(result, None);
+
+        let p1 = Point2::new(0.0, 0.0);
+        let p2 = Point2::new(10.0, 10.0);
+        let p3 = Point2::new(0.0, 5.0);
+        let p4 = Point2::new(10.0, 5.0);
+        let s1 = Segment2::new(p1, p2);
+        let s2 = Segment2::new(p3, p4);
+        let result = s1.intersection(&s2, None);
+        assert_eq!(result, Some(Point2::new(5.0, 5.0)));
+
+        let p1 = Point2::new(0.0, 0.0);
+        let p2 = Point2::new(10.0, 10.0);
+        let p3 = Point2::new(0.0, 5.0);
+        let p4 = Point2::new(10.0, 0.0);
+        let s1 = Segment2::new(p1, p2);
+        let s2 = Segment2::new(p3, p4);
+        let result = s1.intersection(&s2, None);
+        assert_eq!(result, Some(Point2::new(3.333333, 3.333333)));
     }
 }
