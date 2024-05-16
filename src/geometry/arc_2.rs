e16e0662a62b7e6f689d9d17a9cef17e4c1775e1
liuyulvv <liuyulvv@outlook.com>
2024-05-16 16:40:38 +0800

    fix: use radian in Arc2

--- src/geometry/arc_2.rs
+++ src/geometry/arc_2.rs
@@ -2,16 +2,16 @@ use super::circle_2::Circle2;
 
 pub struct Arc2 {
     pub circle: Circle2,
-    pub start_angle: f64,
-    pub end_angle: f64,
+    pub start_radian: f64,
+    pub end_radian: f64,
 }
 
 impl Arc2 {
-    pub fn new(circle: Circle2, start_angle: f64, end_angle: f64) -> Self {
+    pub fn new(circle: Circle2, start_radian: f64, end_radian: f64) -> Self {
         Self {
             circle,
-            start_angle,
-            end_angle,
+            start_radian,
+            end_radian,
         }
     }
 }
@@ -20,8 +20,8 @@ impl Default for Arc2 {
     fn default() -> Self {
         Self {
             circle: Circle2::default(),
-            start_angle: 0.0,
-            end_angle: std::f64::consts::PI,
+            start_radian: 0.0,
+            end_radian: std::f64::consts::PI,
         }
     }
 }
