43e3ce2d92c3155270f65197779c8b6a4bdd712e
liuyulvv <liuyulvv@outlook.com>
2024-05-17 10:11:30 +0800

    fix: intersection segment_2_circle_2

--- src/algorithm/projection/point_2_segment_2.rs
+++ src/algorithm/projection/point_2_segment_2.rs
@@ -13,7 +13,7 @@ pub fn point_2_project_segment_2(
     let w = *point_2 - segment_2.start;
     let c1 = w.dot(&v);
     let c2 = v.dot(&v);
-    if c1 < eps || c1 > c2 - eps {
+    if c1 < -eps || c1 > c2 + eps {
         return None;
     }
     let b = c1 / c2;
@@ -26,6 +26,11 @@ mod tests {
 
     #[test]
     fn point_2_segment_2() {
+        let point_2 = Point2::new(0.0, 0.0);
+        let segment_2 = Segment2::new(Point2::new(0.0, 0.0), Point2::new(2.0, 2.0));
+        let result = point_2_project_segment_2(&point_2, &segment_2, None);
+        assert_eq!(result, Some(Point2::new(0.0, 0.0)));
+
         let point_2 = Point2::new(1.0, 1.0);
         let segment_2 = Segment2::new(Point2::new(0.0, 0.0), Point2::new(2.0, 2.0));
         let result = point_2_project_segment_2(&point_2, &segment_2, None);
