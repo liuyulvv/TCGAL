a73ea58fa6d8a37bc8438958a577f1f4ec51adab
liuyulvv <liuyulvv@outlook.com>
2024-05-20 17:31:56 +0800

    del: topo

--- src/topo/arrangement_2.rs
+++
@@ -1,7 +0,0 @@
-use super::{edge_2::Edge2, face_2::Face2, vertex_2::Vertex2};
-
-pub struct Arrangement2 {
-    pub vertices: Vec<Vertex2>,
-    pub edges: Vec<Edge2>,
-    pub faces: Vec<Face2>,
-}
--- src/topo/face_2.rs
+++
@@ -1,5 +0,0 @@
-use super::edge_2::Edge2;
-
-pub struct Face2 {
-    pub edges: Option<Vec<Box<Edge2>>>,
-}
