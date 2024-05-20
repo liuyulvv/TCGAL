fa451255a3156bdeb2913eff68926a2dcb6a7bff
liuyulvv <liuyulvv@outlook.com>
2024-05-20 23:08:14 +0800

    del: test

--- src/kernel/simple_cartesian/vector_2.rs
+++ src/kernel/simple_cartesian/vector_2.rs
@@ -99,27 +99,3 @@ impl<T: BaseNumberTypeTrait> PartialEq for Vector2<T> {
         !self.eq(other)
     }
 }
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn test_vector_2() {
-        let vector_2_f32 = Vector2::new(1.0f32, 2.0f32);
-        assert_eq!(vector_2_f32.length(), 5.0f32.sqrt());
-
-        let vector_2_f64 = Vector2::new(1.0f64, 2.0f64);
-        assert_eq!(vector_2_f64.length(), 5.0f64.sqrt());
-
-        let vector_2_f64_a = Vector2::new(1.0, 2.0);
-        let vector_2_f64_b = Vector2::new(3.0, 4.0);
-        let vector_2_f64_add = vector_2_f64_a + vector_2_f64_b;
-        let vector_2_f64_sub = vector_2_f64_a - vector_2_f64_b;
-        let vector_2_f64_mul = vector_2_f64_a * 2.0;
-        assert_eq!(vector_2_f64_add, Vector2::new(4.0, 6.0));
-        assert_eq!(vector_2_f64_sub, Vector2::new(-2.0, -2.0));
-        assert_eq!(vector_2_f64_mul, Vector2::new(2.0, 4.0));
-    }
-}
