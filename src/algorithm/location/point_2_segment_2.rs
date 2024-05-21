65c50dcac4bfac9bf16726f8a8853b9a735b6f29
liuyulvv <liuyulvv@outlook.com>
2024-05-21 23:10:36 +0800

    add: locate_point_2_segment_2

--- src/algorithm/location/point_2_segment_2.rs
+++ src/algorithm/location/point_2_segment_2.rs
@@ -5,6 +5,14 @@ use crate::{
     number_type::base_number_type_trait::BaseNumberTypeTrait,
 };
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, PartialOrd, Ord)]
+pub enum Point2Segment2Location {
+    ON,
+    LEFT,
+    RIGHT,
+    COLLINEAR,
+}
+
 pub fn is_point_2_on_segment_2<'a, NT, T>(point_2: T::Point2, segment_2: T) -> bool
 where
     T: BaseSegment2<'a, NT>,
@@ -25,6 +33,35 @@ where
         && dot_ab_ac <= dot_ab_ab
 }
 
+pub fn locate_point_2_segment_2<'a, NT, T>(
+    point_2: T::Point2,
+    segment_2: T,
+) -> Point2Segment2Location
+where
+    T: BaseSegment2<'a, NT>,
+    T::Point2: BasePoint2<NT>,
+    NT: BaseNumberTypeTrait,
+{
+    let source = segment_2.source();
+    let target = segment_2.target();
+    let vec_ab = target - source;
+    let vec_ac = point_2 - source;
+    let area = vec_ab.cross(&vec_ac);
+    if area > NT::default_eps() {
+        Point2Segment2Location::LEFT
+    } else if area < -NT::default_eps() {
+        Point2Segment2Location::RIGHT
+    } else {
+        let dot_ab_ac = vec_ab.dot(&vec_ac);
+        let dot_ab_ab = vec_ab.dot(&vec_ab);
+        if dot_ab_ac <= dot_ab_ab && dot_ab_ac >= -NT::default_eps() {
+            Point2Segment2Location::ON
+        } else {
+            Point2Segment2Location::COLLINEAR
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -34,10 +71,69 @@ mod tests {
 
     #[test]
     fn test_is_point_2_on_segment_2() {
-        let p1 = Point2::new(1.0, 1.0);
+        let p1 = Point2::new(0.0, 0.0);
         let p2 = Point2::new(10.0, 10.0);
         let s = Segment2::new(&p1, &p2);
 
         assert_eq!(is_point_2_on_segment_2(p1, s), true);
+
+        let p1 = Point2::new(0.0, 0.0);
+        let p2 = Point2::new(10.0, 10.0);
+        let s = Segment2::new(&p1, &p2);
+
+        let p3 = Point2::new(5.0, 5.0);
+        assert_eq!(is_point_2_on_segment_2(p3, s), true);
+
+        let p3 = Point2::new(5.0, 5.1);
+        assert_eq!(is_point_2_on_segment_2(p3, s), false);
+
+        let p3 = Point2::new(f64::sqrt(3.0), f64::sqrt(3.0));
+        assert_eq!(is_point_2_on_segment_2(p3, s), true);
+    }
+
+    #[test]
+    fn test_locate_point_2_segment_2() {
+        let p1 = Point2::new(0.0, 0.0);
+        let p2 = Point2::new(10.0, 10.0);
+        let s = Segment2::new(&p1, &p2);
+
+        let p3 = Point2::new(5.0, 5.0);
+        assert_eq!(locate_point_2_segment_2(p3, s), Point2Segment2Location::ON);
+
+        let p3 = Point2::new(5.0, 5.1);
+        assert_eq!(
+            locate_point_2_segment_2(p3, s),
+            Point2Segment2Location::LEFT
+        );
+
+        let p3 = Point2::new(5.0, 4.9);
+        assert_eq!(
+            locate_point_2_segment_2(p3, s),
+            Point2Segment2Location::RIGHT
+        );
+
+        let p3 = Point2::new(0.0, 0.0);
+        assert_eq!(locate_point_2_segment_2(p3, s), Point2Segment2Location::ON);
+
+        let p3 = Point2::new(10.0, 10.0);
+        assert_eq!(locate_point_2_segment_2(p3, s), Point2Segment2Location::ON);
+
+        let p3 = Point2::new(10.0, 10.1);
+        assert_eq!(
+            locate_point_2_segment_2(p3, s),
+            Point2Segment2Location::LEFT
+        );
+
+        let p3 = Point2::new(10.0, 9.9);
+        assert_eq!(
+            locate_point_2_segment_2(p3, s),
+            Point2Segment2Location::RIGHT
+        );
+
+        let p3 = Point2::new(-10.0, -10.0);
+        assert_eq!(
+            locate_point_2_segment_2(p3, s),
+            Point2Segment2Location::COLLINEAR
+        );
     }
 }
