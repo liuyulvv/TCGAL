098921d484f879dc81b06463c4ef75ea0d9326c9
liuyulvv <liuyulvv@outlook.com>
2024-06-06 11:31:04 +0800

    update: point_2 ord

--- src/kernel/point_2.rs
+++ src/kernel/point_2.rs
@@ -3,75 +3,81 @@ use std::ops::{Add, Sub};
 use super::{number_type::NumberType, vector_2::Vector2};
 
 #[derive(Debug, Clone, Copy)]
-pub struct Point2<NT: NumberType> {
-    x: NT,
-    y: NT,
+pub struct Point2<T: NumberType> {
+    x: T,
+    y: T,
 }
 
-impl<NT: NumberType> Point2<NT> {
-    pub fn new(x: NT, y: NT) -> Self {
+impl<T: NumberType> Point2<T> {
+    pub fn new(x: T, y: T) -> Self {
         Self { x, y }
     }
 
-    pub fn x(&self) -> NT {
+    pub fn x(&self) -> T {
         self.x
     }
 
-    pub fn y(&self) -> NT {
+    pub fn y(&self) -> T {
         self.y
     }
 
     pub fn equals(&self, other: &Self) -> bool {
-        let eps = NT::default_eps();
+        let eps = T::default_eps();
         (self.x - other.x).abs() < eps && (self.y - other.y).abs() < eps
     }
 
-    pub fn get_vector(&self) -> Vector2<NT> {
+    pub fn get_vector(&self) -> Vector2<T> {
         Vector2::new(self.x, self.y)
     }
 }
 
-impl<NT: NumberType> Add for Point2<NT> {
-    type Output = Vector2<NT>;
+impl<T: NumberType> Add for Point2<T> {
+    type Output = Vector2<T>;
 
     fn add(self, other: Self) -> Self::Output {
         Vector2::new(self.x + other.x(), self.y + other.y())
     }
 }
 
-impl<NT: NumberType> Sub for Point2<NT> {
-    type Output = Vector2<NT>;
+impl<T: NumberType> Sub for Point2<T> {
+    type Output = Vector2<T>;
 
     fn sub(self, other: Self) -> Self::Output {
         Vector2::new(self.x - other.x(), self.y - other.y())
     }
 }
 
-impl<NT: NumberType> PartialEq for Point2<NT> {
+impl<T: NumberType> Eq for Point2<T> {}
+
+impl<T: NumberType> PartialEq for Point2<T> {
     fn eq(&self, other: &Self) -> bool {
         self.x == other.x && self.y == other.y
     }
+}
 
-    fn ne(&self, other: &Self) -> bool {
-        !self.eq(other)
+impl<T: NumberType> Ord for Point2<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.equals(other) {
+            return std::cmp::Ordering::Equal;
+        }
+        if self.y > other.y {
+            return std::cmp::Ordering::Greater;
+        } else if self.y < other.y {
+            return std::cmp::Ordering::Less;
+        } else {
+            if self.x > other.x {
+                return std::cmp::Ordering::Less;
+            } else if self.x < other.x {
+                return std::cmp::Ordering::Greater;
+            } else {
+                return std::cmp::Ordering::Equal;
+            }
+        }
     }
 }
 
-impl<NT: NumberType> PartialOrd for Point2<NT> {
+impl<T: NumberType> PartialOrd for Point2<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        if self.equals(other) {
-            return Some(std::cmp::Ordering::Equal);
-        }
-        if self.x < other.x {
-            Some(std::cmp::Ordering::Less)
-        } else if self.x > other.x {
-            Some(std::cmp::Ordering::Greater)
-        } else if self.y < other.y {
-            Some(std::cmp::Ordering::Less)
-        } else if self.y > other.y {
-            Some(std::cmp::Ordering::Greater)
-        } else {
-            Some(std::cmp::Ordering::Equal)
-        }
+        Some(self.cmp(other))
     }
 }
