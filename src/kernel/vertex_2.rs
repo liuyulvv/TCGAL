97bd45f51be68ba7da509877bebf03b29d508b44
liuyulvv <liuyulvv@outlook.com>
2024-06-07 15:12:43 +0800

    style

--- src/kernel/vertex_2.rs
+++ src/kernel/vertex_2.rs
@@ -3,14 +3,14 @@ use std::{cell::RefCell, rc::Rc};
 use super::{edge_2::Edge2, number_type::NumberType, point_2::Point2};
 
 #[derive(Debug, Clone)]
-pub struct Vertex2<NT: NumberType> {
-    x: NT,
-    y: NT,
-    edges: Vec<Rc<RefCell<Edge2<NT>>>>,
+pub struct Vertex2<T: NumberType> {
+    x: T,
+    y: T,
+    edges: Vec<Rc<RefCell<Edge2<T>>>>,
 }
 
-impl<NT: NumberType> Vertex2<NT> {
-    pub fn new(x: NT, y: NT) -> Self {
+impl<T: NumberType> Vertex2<T> {
+    pub fn new(x: T, y: T) -> Self {
         Self {
             x,
             y,
@@ -18,44 +18,44 @@ impl<NT: NumberType> Vertex2<NT> {
         }
     }
 
-    pub fn x(&self) -> NT {
+    pub fn x(&self) -> T {
         self.x
     }
 
-    pub fn y(&self) -> NT {
+    pub fn y(&self) -> T {
         self.y
     }
 
-    pub fn edges(&self) -> Vec<Rc<RefCell<Edge2<NT>>>> {
+    pub fn edges(&self) -> Vec<Rc<RefCell<Edge2<T>>>> {
         self.edges.clone()
     }
 
-    pub fn add_edge(&mut self, edge: Rc<RefCell<Edge2<NT>>>) {
+    pub fn add_edge(&mut self, edge: Rc<RefCell<Edge2<T>>>) {
         self.edges.push(edge);
     }
 
-    pub fn remove_edge(&mut self, edge: Rc<RefCell<Edge2<NT>>>) {
+    pub fn remove_edge(&mut self, edge: Rc<RefCell<Edge2<T>>>) {
         self.edges.retain(|e| e != &edge);
     }
 
     pub fn equals(&self, other: &Self) -> bool {
-        let eps = NT::default_eps();
+        let eps = T::default_eps();
         ((self.x - other.x) * (self.x - other.x) + (self.y - other.y) * (self.y - other.y)).sqrt()
             < eps
     }
 
-    pub fn to_point(&self) -> Point2<NT> {
+    pub fn to_point(&self) -> Point2<T> {
         Point2::new(self.x, self.y)
     }
 }
 
-impl<'a, NT: NumberType> PartialEq for Vertex2<NT> {
+impl<'a, T: NumberType> PartialEq for Vertex2<T> {
     fn eq(&self, other: &Self) -> bool {
         self.equals(other)
     }
 }
 
-impl<'a, NT: NumberType> PartialOrd for Vertex2<NT> {
+impl<'a, T: NumberType> PartialOrd for Vertex2<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         if self.y() > other.y() {
             return Some(std::cmp::Ordering::Greater);
