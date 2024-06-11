a3fb3e7dd8f5bbbb9ec7da12b93902c1de46769f
liuyulvv <liuyulvv@outlook.com>
2024-06-11 22:54:11 +0800

    todo: intersection segment_2_ray_2

--- src/algorithm/intersection.rs
+++ src/algorithm/intersection.rs
@@ -1,4 +1,5 @@
 pub mod edge_2_edge_2;
 pub mod segment_2_circle_2;
+pub mod segment_2_ray_2;
 pub mod segment_2_segment_2;
 pub mod sweep_line_segment_2_intersection;
---
+++ src/algorithm/intersection/segment_2_ray_2.rs
@@ -0,0 +1,5 @@
+use crate::kernel::{number_type::NumberType, ray_2::Ray2, segment_2::Segment2};
+
+pub fn is_segment_2_ray_2_intersected<T: NumberType>(s: &Segment2<T>, r: &Ray2<T>) -> bool {
+    todo!()
+}
