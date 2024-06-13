7db1bfb1a684bf9ed8403633b93851302b492816
liuyulvv <liuyulvv@outlook.com>
2024-06-13 15:20:18 +0800

    add: location point_2_polygon_2

--- src/algorithm/location/point_2_polygon_2.rs
+++ src/algorithm/location/point_2_polygon_2.rs
@@ -1,6 +1,9 @@
 use crate::kernel::{number_type::NumberType, point_2::Point2, polygon_2::Polygon2};
 
-use super::location_enum::Point2Polygon2Location;
+use super::{
+    location_enum::{Point2Polygon2Location, Point2Segment2Location},
+    point_2_segment_2::locate_point_2_segment_2,
+};
 
 pub fn is_point_2_on_polygon_2<T: NumberType>(point: &Point2<T>, polygon: &Polygon2<T>) -> bool {
     let location = locate_point_2_polygon_2(point, polygon);
@@ -36,5 +39,37 @@ pub fn locate_point_2_polygon_2<T: NumberType>(
     point: &Point2<T>,
     polygon: &Polygon2<T>,
 ) -> Point2Polygon2Location {
-    todo!()
+    let edges = polygon.edges();
+    let mut location = None;
+    for edge in &edges {
+        let edge_location = locate_point_2_segment_2(point, edge);
+        match edge_location {
+            Point2Segment2Location::On => return Point2Polygon2Location::On,
+            Point2Segment2Location::Left => match location {
+                Some(Point2Segment2Location::Right) => {
+                    return Point2Polygon2Location::Outside;
+                }
+                None => {
+                    location = Some(Point2Segment2Location::Left);
+                }
+                _ => {}
+            },
+            Point2Segment2Location::Right => match location {
+                Some(Point2Segment2Location::Left) => {
+                    return Point2Polygon2Location::Outside;
+                }
+                None => {
+                    location = Some(Point2Segment2Location::Right);
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+    match location {
+        None => return Point2Polygon2Location::Outside,
+        _ => {
+            return Point2Polygon2Location::Inside;
+        }
+    }
 }
