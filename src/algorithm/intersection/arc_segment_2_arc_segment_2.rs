1ff6a7af6071fe0399a36678a660462a39a616ff
liuyulvv <liuyulvv@outlook.com>
2024-06-18 18:01:22 +0800

    todo: arc_segment_2_arc_segment_2

---
+++ src/algorithm/intersection/arc_segment_2_arc_segment_2.rs
@@ -0,0 +1,8 @@
+use crate::kernel::{number_type::NumberType, segment_2::Segment2};
+
+pub fn is_arc_segment_2_arc_segment_2_intersected<T: NumberType>(
+    arc_segment_a: &impl Segment2<T>,
+    arc_segment_b: &impl Segment2<T>,
+) -> bool {
+    todo!()
+}
