d6c6cd8b52ceca49c235260f56683d3a001374ba
liuyulvv <liuyulvv@outlook.com>
2024-06-18 18:38:36 +0800

    todo: point_2_arc_segment_2

---
+++ src/algorithm/location/point_2_arc_segment_2.rs
@@ -0,0 +1,37 @@
+use crate::kernel::{
+    arc_segment_2::ArcSegment2, number_type::NumberType, point_2::Point2, segment_2::Segment2,
+    util_enum::Orientation,
+};
+
+use super::location_enum::Point2ArcSegment2Location;
+
+pub fn locate_point_2_arc_segment_2<T: NumberType>(
+    point: &Point2<T>,
+    arc_segment: &ArcSegment2<T>,
+) -> Point2ArcSegment2Location {
+    let center = arc_segment.center();
+    let radius = arc_segment.radius();
+    let distance = center.distance(point);
+    if distance.equals(radius) {
+        let vector = *point - center;
+        let source = arc_segment.source() - center;
+        let target = arc_segment.target() - center;
+        match arc_segment.orientation() {
+            Orientation::CounterClockwise => {
+                let radian = source.radian_to(&target);
+                let radian_source = vector.radian_to(&source);
+                let radian_target = vector.radian_to(&target);
+                let source_radian = source.radian_to(&vector);
+                if radian_source.equals(T::zero()) || radian_target.equals(T::zero()) {
+                    Point2ArcSegment2Location::On
+                }
+                todo!()
+            }
+            Orientation::Clockwise => {
+                todo!()
+            }
+        }
+    } else {
+        Point2ArcSegment2Location::NotOn
+    }
+}
