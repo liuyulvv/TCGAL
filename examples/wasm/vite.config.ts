d5f6d59d96166a1aa7dd31ca3cd6f39e1456b622
liuyulvv <liuyulvv@outlook.com>
2024-07-17 22:27:52 +0800

    del: examples

--- examples/wasm/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), wasm()],
-});
