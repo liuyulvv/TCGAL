ee1a3b889c1bf34ee905bca799177b0f5d795939
liuyulvv <liuyulvv@outlook.com>
2024-07-20 20:58:58 +0800

    add: location point2 line2

--- src/algorithm/location.rs
+++ src/algorithm/location.rs
@@ -1,6 +1,7 @@
 pub mod location_enum;
 pub mod point_2_arc_segment_2;
 pub mod point_2_circle_segment_2;
+pub mod point_2_line_2;
 pub mod point_2_line_segment_2;
 pub mod point_2_polygon_2;
 pub mod point_2_ray_2;
